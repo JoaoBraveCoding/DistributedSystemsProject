bb505bf18a716bef1330dfd731fa6303a942351c
Lídia Freitas <lidiamcfreitas@gmail.com>
2016-04-11 19:37:14 +0100

    added changes in states

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -33,6 +33,7 @@ public class BrokerPort implements BrokerPortType {
   private List<TransporterPortType> transporters = new ArrayList<TransporterPortType>();
   private List<TransportView>    	transports   = new ArrayList<TransportView>();
   private Map<String, String> places = new HashMap<String, String>();
+  private int identifierCounter = 0;
   
   public BrokerPort() {
 	  super();
@@ -85,15 +86,23 @@ public class BrokerPort implements BrokerPortType {
 	  // check if price is positive
 	  priceIsValid(price);
 
+	  // find best job
+	  TransportView transportView = null;
 	  int bestPrice = price;
 	  JobView bestJob = null;
 	  TransporterPortType bestTransporter = null;
 	  Map<JobView, TransporterPortType> badJobs = new HashMap<JobView, TransporterPortType>();
+	  Map<JobView, TransportView> badTransports = new HashMap<JobView, TransportView>();
 	  
 	  for(TransporterPortType transporter: transporters){
 		  try {
+			  // create transport for each transporter
+			  transportView = createTransport(origin, destination, price, bestTransporter);
 			  JobView job = transporter.requestJob(origin, destination, price);
 			  badJobs.put(job, transporter);
+			  badTransports.put(job, transportView);
+			  
+			  transportView.setState(TransportStateView.BUDGETED);
 			  if (job == null) continue; // TODO should I tell that it is rejected?
 			  if (job.getJobPrice() <= bestPrice){
 				  bestJob = job;
@@ -112,11 +121,18 @@ public class BrokerPort implements BrokerPortType {
 	  // TODO which exception?
 	  if (bestJob == null) throw new UnavailableTransportFault_Exception(destination, null);
 	  badJobs.remove(bestJob);
+	  
 	  try {
-		 bestTransporter.decideJob(bestJob.getJobIdentifier(), true);
-		 badJobs.remove(bestJob.getJobIdentifier());
+		 JobView stateJob = bestTransporter.decideJob(bestJob.getJobIdentifier(), true);
+		 if(stateJob.getJobState() == JobStateView.ACCEPTED){
+			 badTransports.get(bestJob).setState(TransportStateView.BOOKED);;
+		 }
+		 
 		 for(JobView j: badJobs.keySet()){
-			 badJobs.get(j).decideJob(j.getJobIdentifier(), false);
+			 stateJob = badJobs.get(j).decideJob(j.getJobIdentifier(), false);
+			 if(stateJob.getJobState() == JobStateView.REJECTED){
+				 badTransports.get(bestJob).setState(TransportStateView.FAILED);;
+			 }
 		 }
 	  } catch (BadJobFault_Exception e) {
 		// TODO do what?
@@ -124,8 +140,23 @@ public class BrokerPort implements BrokerPortType {
 	  return null;
   }
   
-  // Check if place exists
-  public void placeExists(String place) throws UnknownLocationFault_Exception{
+  private TransportView createTransport(String origin, String destination, int price, TransporterPortType transporter) {
+	TransportView transport = new TransportView();
+	
+	transport.setId(getIdentifier());
+	transport.setOrigin(origin);
+	transport.setDestination(destination);
+	transport.setPrice(price);
+	transport.setTransporterCompany(getTransporterPortTypeName(transporter));
+	transport.setState(TransportStateView.REQUESTED);
+	
+	transports.add(transport);
+	return transport;
+}
+
+
+// Check if place exists
+  private void placeExists(String place) throws UnknownLocationFault_Exception{
 	  if(places.containsKey(place)) return;
 	  
 	  UnknownLocationFault fault = new UnknownLocationFault();
@@ -134,13 +165,27 @@ public class BrokerPort implements BrokerPortType {
   }
   
   // Check if price is valid
-  public void priceIsValid(int price) throws InvalidPriceFault_Exception{
+  private void priceIsValid(int price) throws InvalidPriceFault_Exception{
 	  if (price < 0) {
 		  InvalidPriceFault fault = new InvalidPriceFault();
 		  fault.setPrice(price);
 		  throw new InvalidPriceFault_Exception("Price invalid. ", fault);
 	  }
 	  return;
+  }
+  
+  private String getIdentifier(){
+	  identifierCounter++;
+	  return "" + identifierCounter;
+  }
+  
+  private String getTransporterPortTypeName(TransporterPortType transporter){
+	  for (Integer i = 0; i < transporters.size(); i++){
+		  if(transporter == transporters.get(i)){
+			  return "Transporter" + i.toString();
+		  }
+	  }
+	  return "";
   }
 
   @Override
@@ -156,6 +201,9 @@ public class BrokerPort implements BrokerPortType {
 
   @Override
   public void clearTransports() {
+	for(TransportView transport: transports){
+		transport.getTransporterCompany();
+	}
     // TODO Auto-generated method stub
     
   }
