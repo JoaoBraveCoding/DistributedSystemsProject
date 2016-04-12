97f9da5835d24032f9425615470c8eb2d8e16cbf
José Semedo <jose.francisco.semedo@gmail.com>
2016-04-12 18:08:58 +0100

    requestJob tests complete.

--- transporter-ws/src/test/java/pt/upa/transporter/ws/TransporterTest.java
+++ transporter-ws/src/test/java/pt/upa/transporter/ws/TransporterTest.java
@@ -6,6 +6,7 @@ import pt.upa.transporter.ws.JobView;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,14 +14,19 @@ import java.util.List;
 public class TransporterTest extends AbstractTransporterTest{
 
   TransporterPort transporter;
+  TransporterPort transporter2;
   List<JobView> jobViews = new ArrayList<JobView>();
   
   @Override
   protected void populate(){
     transporter = new TransporterPort("UpaTransporter1");
-    JobView jvw = createJobView("UpaTransporter1", "UpaTransporter1.0", "Beja", "Faro", 50, 5); //PROPOSED
-    jobViews.add(jvw);
+    transporter2 = new TransporterPort("UpaTransporter2");
     
+    JobView jvw = createJobView("UpaTransporter1", "UpaTransporter1.0", "Beja", "Faro", 50, 5); //PROPOSED
+    jobViews.add(jvw); //0
+    jvw = createJobView("UpaTransporter2", "UpaTransporter2.0", "Porto", "Braga", 50, 5); //PROPOSED
+    jobViews.add(jvw); //1
+
   }
   
   @Test
@@ -29,12 +35,59 @@ public class TransporterTest extends AbstractTransporterTest{
     assertEquals("Pong Test!", result);
   }
   
-  //assert within 5 seconds because of jobState
   @Test
   public void good_job_request() throws BadLocationFault_Exception, BadPriceFault_Exception{
     JobView jv2 = transporter.requestJob("Beja", "Faro", 50);
-    assertEquals(true, viewsEquals(jv2, 0));
+    assertTrue(viewsEquals(jv2, 0));
   }
+
+  @Test
+  public void good_job_request_transp2() throws BadLocationFault_Exception, BadPriceFault_Exception{
+    JobView jv2 = transporter2.requestJob("Porto", "Braga", 50);
+    assertTrue(viewsEquals(jv2, 1));
+  }
+
+  @Test (expected = BadLocationFault_Exception.class)
+  public void job_request_with_empty_origin() throws BadLocationFault_Exception, 
+  BadPriceFault_Exception{
+    transporter.requestJob("", "Beja", 50);
+  }
+  
+  @Test (expected = BadLocationFault_Exception.class)
+  public void job_request_with_empty_destination() throws BadLocationFault_Exception, 
+  BadPriceFault_Exception{
+    transporter.requestJob("Beja", "", 50);
+  }
+  
+  @Test
+  public void job_request_with_not_operable_origin() throws BadLocationFault_Exception, 
+  BadPriceFault_Exception{
+    JobView jv3 = transporter.requestJob("Porto", "Beja", 50);
+    assertNull(jv3);
+  }
+  
+  @Test
+  public void job_request_with_not_operable_destination() throws BadLocationFault_Exception, 
+  BadPriceFault_Exception{
+    JobView jv3 = transporter.requestJob("Beja", "Porto", 50);
+    assertNull(jv3);
+  }
+  
+  @Test
+  public void overpriced_job_request() throws BadLocationFault_Exception, 
+  BadPriceFault_Exception{
+    JobView jv3 = transporter.requestJob("Beja", "Faro", 120);
+    assertNull(jv3);
+  }
+
+  @Test
+  public void underpriced_job_request() throws BadLocationFault_Exception,
+  BadPriceFault_Exception{
+    JobView jv3 = transporter.requestJob("Beja",  "Faro",  5);
+    assertTrue(jv3.getJobPrice() < 5 && jv3.getJobPrice() >= 0);
+  }
+  
+
 
   private boolean viewsEquals(JobView jv, int i){
     JobView expected = jobViews.get(i);
@@ -42,7 +95,7 @@ public class TransporterTest extends AbstractTransporterTest{
        jv.getCompanyName().equals(expected.getCompanyName()) &&
        jv.getJobIdentifier().equals(expected.getJobIdentifier()) &&
        jv.getJobOrigin().equals(expected.getJobOrigin()) &&
-       jv.getJobPrice() > 10 && jv.getJobPrice() <= 100 &&
+       expected.getJobPrice() > 10 && expected.getJobPrice() <= 100 &&
        jv.getJobState().equals(expected.getJobState())){
       return true;
     }
