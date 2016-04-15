d0ca1e5c0c7fe2366b8023ded184fc538761550e
Lídia Freitas <lidiamcfreitas@gmail.com>
2016-04-15 21:27:27 +0100

    added branch client tests

---
+++ broker-ws-cli/src/test/java/pt/upa/broker/ws/it/RequestTransportIT.java
@@ -0,0 +1,37 @@
+package pt.upa.broker.ws.it;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import pt.upa.broker.ws.InvalidPriceFault_Exception;
+import pt.upa.broker.ws.TransportStateView;
+import pt.upa.broker.ws.TransportView;
+import pt.upa.broker.ws.UnavailableTransportFault_Exception;
+import pt.upa.broker.ws.UnavailableTransportPriceFault_Exception;
+import pt.upa.broker.ws.UnknownLocationFault_Exception;
+import pt.upa.broker.ws.UnknownTransportFault_Exception;
+
+public class RequestTransportIT extends AbstractBrokerIT{
+ 
+	@Test(expected = InvalidPriceFault_Exception.class)
+	  public void broker_viewTransport_incorrectPrice() throws UnknownTransportFault_Exception, 
+	  InvalidPriceFault_Exception, UnavailableTransportFault_Exception, UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
+		client.requestTransport("Lisboa", "Coimbra", -10);
+	  }
+	
+	@Test(expected = UnknownLocationFault_Exception.class)
+	  public void broker_viewTransport_incorrectDestiny() throws UnknownTransportFault_Exception, 
+	  InvalidPriceFault_Exception, UnavailableTransportFault_Exception, UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
+		client.requestTransport("Lisboa", "Japão", 49);
+	  }
+	
+	@Test(expected = UnknownLocationFault_Exception.class)
+	  public void broker_viewTransport_incorrectOrigin() throws UnknownTransportFault_Exception, 
+	  InvalidPriceFault_Exception, UnavailableTransportFault_Exception, UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
+		client.requestTransport("Japão","Lisboa", 49);
+	  }
+  
+	
+}
---
+++ broker-ws-cli/src/test/java/pt/upa/broker/ws/it/viewTransportIT.java
@@ -0,0 +1,28 @@
+package pt.upa.broker.ws.it;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import pt.upa.broker.ws.InvalidPriceFault_Exception;
+import pt.upa.broker.ws.TransportStateView;
+import pt.upa.broker.ws.TransportView;
+import pt.upa.broker.ws.UnavailableTransportFault_Exception;
+import pt.upa.broker.ws.UnavailableTransportPriceFault_Exception;
+import pt.upa.broker.ws.UnknownLocationFault_Exception;
+import pt.upa.broker.ws.UnknownTransportFault_Exception;
+
+public class viewTransportIT extends AbstractBrokerIT{
+ 
+  @Test
+  public void broker_viewTransport_success() throws UnknownTransportFault_Exception, InvalidPriceFault_Exception, UnavailableTransportFault_Exception, UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
+	  String id = "";
+		id = client.requestTransport("Lisboa", "Coimbra", 49);
+	System.out.println("TEST: " + id);
+	TransportView transport = client.viewTransport(id);
+    assertEquals(transport.getState(), TransportStateView.BOOKED);
+  }
+  
+}
