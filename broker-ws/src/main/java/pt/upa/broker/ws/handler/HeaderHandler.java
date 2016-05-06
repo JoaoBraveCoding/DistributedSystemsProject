f79bcb0dbc9e4cb9a16d4c7b7556881da47c0ede
JoaoBraveCoding <joao.marcal12@hotmail.com>
2016-05-06 19:47:45 +0100

    Removed aux functions from the hander and put then in SecurityFunctions

--- broker-ws/src/main/java/pt/upa/broker/ws/handler/HeaderHandler.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/handler/HeaderHandler.java
@@ -1,30 +1,17 @@
 package pt.upa.broker.ws.handler;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.ReadOnlyFileSystemException;
-import java.security.InvalidKeyException;
-import java.security.Key;
+
 import java.security.KeyFactory;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
-import java.security.Signature;
-import java.security.SignatureException;
 import java.security.cert.Certificate;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Iterator;
 import java.util.Set;
 
-import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
 import javax.xml.namespace.QName;
 import javax.xml.soap.Name;
-import javax.xml.soap.Node;
 import javax.xml.soap.SOAPBody;
 import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPEnvelope;
@@ -33,15 +20,12 @@ import javax.xml.soap.SOAPHeaderElement;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.soap.SOAPPart;
 import javax.xml.ws.handler.MessageContext;
-import javax.xml.ws.handler.MessageContext.Scope;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
 
-import com.sun.xml.messaging.saaj.util.Base64;
-
-
+import pt.upa.ws.SecurityFunctions;
 import example.crypto.X509DigitalSignature;
-import static javax.xml.bind.DatatypeConverter.printHexBinary;
+import static javax.xml.bind.DatatypeConverter.printBase64Binary;
 import static javax.xml.bind.DatatypeConverter.parseBase64Binary;
 
 /**
@@ -97,11 +81,11 @@ public class HeaderHandler implements SOAPHandler<SOAPMessageContext> {
         random.nextBytes(nonce);
 
         //make digest
-        byte[] digest = digestBroker(plainText, nonce);
+        byte[] digest = SecurityFunctions.digestBroker(plainText, nonce);
 
         //encrypt digest = signature
 
-        PrivateKey privKey = (PrivateKey) getKey("../../../../../../../../keys/UpaBrokerPriv.key");
+        PrivateKey privKey = (PrivateKey) SecurityFunctions.getKey("../../../../../../../../keys/UpaBrokerPriv.key");
         byte[] signature = X509DigitalSignature.makeDigitalSignature(digest, privKey);
 
         // add header
@@ -110,7 +94,7 @@ public class HeaderHandler implements SOAPHandler<SOAPMessageContext> {
           sh = se.addHeader();
 
         //turn signature into text
-        String textSignature = printHexBinary(signature);
+        String textSignature = printBase64Binary(signature);
 
         // get first header element TODO what's with the e?
         Name signatureName = se.createName("signature", "e", "urn:upa");
@@ -118,7 +102,7 @@ public class HeaderHandler implements SOAPHandler<SOAPMessageContext> {
         elementHeader.addTextNode(textSignature);
 
         //turn signature into text
-        String textNonce = printHexBinary(nonce);
+        String textNonce = printBase64Binary(nonce);
 
         Name nonceName = se.createName("nonce", "e", "urn:upa");
         SOAPHeaderElement nonceElement = sh.addHeaderElement(nonceName);
@@ -128,7 +112,7 @@ public class HeaderHandler implements SOAPHandler<SOAPMessageContext> {
         Certificate brokerCertificate = null;//CaPort.requestCertificate("UpaBroker");
 
         //turn certificate into text
-        String textBrokerCertificate = printHexBinary(brokerCertificate.getEncoded());
+        String textBrokerCertificate = printBase64Binary(brokerCertificate.getEncoded());
 
         Name certificateName = se.createName("certificate", "e", "urn:upa");
         SOAPHeaderElement certificateElement = sh.addHeaderElement(certificateName);
@@ -212,16 +196,14 @@ public class HeaderHandler implements SOAPHandler<SOAPMessageContext> {
         String transporterCertificateText = null;//CaPort.requestCertificate("transporterText");
         byte[] transporterCertificate = parseBase64Binary(transporterCertificateText);
 
-        PublicKey pubKeyCA = (PublicKey) getKey("../../../../../../../../keys/CaPub.key");
-        byte[] transporterPublicKey = decryptMessage(pubKeyCA, transporterCertificate);
+        PublicKey pubKeyCA = (PublicKey) SecurityFunctions.getKey("../../../../../../../../keys/CaPub.key");
+        byte[] transporterPublicKey = SecurityFunctions.decryptMessage(pubKeyCA, transporterCertificate);
         PublicKey pubKeyTransporter = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(transporterPublicKey));
 
-
-
-        byte[] computedDigest = digestTransporter(bodyText, nonce, transporterText);
+        byte[] computedDigest = SecurityFunctions.digestTransporter(bodyText, nonce, transporterText);
 
         //Fazer o verify - should the signature be already decrypted or does the function do that?
-        if(verifyDigitalSignature(signature, computedDigest, pubKeyTransporter)){
+        if(SecurityFunctions.verifyDigitalSignature(signature, computedDigest, pubKeyTransporter)){
           System.out.println("Wrong digital signature.");
           return false;
         }
@@ -243,48 +225,6 @@ public class HeaderHandler implements SOAPHandler<SOAPMessageContext> {
     return true;
   }
 
-  private byte[] digestBroker(String msg, byte[] nonce) throws NoSuchAlgorithmException{
-    // get a message digest object using the specified algorithm
-    MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
-    System.out.println(messageDigest.getProvider().getInfo());
-
-    System.out.println("Computing digest ...");
-    byte[] plainBytes = msg.getBytes();
-    byte[] plainPlusNonce = new byte[nonce.length + plainBytes.length];
-    //concat: nonce + message
-    System.arraycopy(nonce, 0, plainPlusNonce, 0, nonce.length);
-    System.arraycopy(plainBytes, 0, plainPlusNonce, nonce.length, plainBytes.length);
-    messageDigest.update(plainPlusNonce);
-    return messageDigest.digest();
-  }
-
-  private byte[] digestTransporter(String msg, byte[] nonce, String TransporterName) throws NoSuchAlgorithmException{
-    // get a message digest object using the specified algorithm
-    MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
-    System.out.println(messageDigest.getProvider().getInfo());
-
-    System.out.println("Computing digest ...");
-    byte[] plainBytes = msg.getBytes();
-    byte[] nameBytes = TransporterName.getBytes();
-    byte[] plainPlusNonceAndName = new byte[nonce.length + plainBytes.length + nameBytes.length];
-    //concat: name + nonce + message
-
-    System.arraycopy(nameBytes, 0, plainPlusNonceAndName, 0, nameBytes.length);
-    System.arraycopy(nonce, 0, plainPlusNonceAndName, nameBytes.length, nonce.length);
-    System.arraycopy(plainBytes, 0, plainPlusNonceAndName, nonce.length, plainBytes.length);
-    messageDigest.update(plainPlusNonceAndName);
-    return messageDigest.digest();
-  }
-
-  private static byte[] readFile(String path) throws FileNotFoundException, IOException {
-    FileInputStream fis = new FileInputStream(path);
-    byte[] content = new byte[fis.available()];
-    fis.read(content);
-    fis.close();
-    return content;
-  }
-
-
   public boolean handleFault(SOAPMessageContext smc) {
     System.out.println("Ignoring fault message...");
     return true;
@@ -293,38 +233,4 @@ public class HeaderHandler implements SOAPHandler<SOAPMessageContext> {
   public void close(MessageContext messageContext) {
   }
 
-  public byte[] decryptMessage(PublicKey publicKey, byte[] message) throws Exception{
-    // get an RSA cipher object
-    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-
-    // decrypt the ciphered digest using the public key
-    cipher.init(Cipher.DECRYPT_MODE, publicKey);
-    return cipher.doFinal(message);
-  }
-
-  public Key getKey(String path) throws Exception{
-    byte[] privEncoded = readFile(path);
-    PKCS8EncodedKeySpec privSpec = new PKCS8EncodedKeySpec(privEncoded);
-    KeyFactory keyFacPriv = KeyFactory.getInstance("RSA");
-    return keyFacPriv.generatePrivate(privSpec);
-  }
-
-  /**
-   * auxiliary method to calculate new digest from text and compare it to the
-   * to deciphered digest
-   */
-  public boolean verifyDigitalSignature(byte[] cipherDigest, byte[] bytes, PublicKey publicKey)
-      throws Exception {
-
-    // verify the signature with the public key
-    Signature sig = Signature.getInstance("SHA1WithRSA");
-    sig.initVerify(publicKey);
-    sig.update(bytes);
-    try {
-      return sig.verify(cipherDigest);
-    } catch (SignatureException se) {
-      System.err.println("Caught exception while verifying signature " + se);
-      return false;
-    }
-  }
 }
--- ws-handlers-aux/src/main/java/pt/upa/ws/SecurityFunctions.java
+++ ws-handlers-aux/src/main/java/pt/upa/ws/SecurityFunctions.java
@@ -1,5 +1,95 @@
 package pt.upa.ws;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+import javax.crypto.Cipher;
+
 public class SecurityFunctions {
   
+  public static byte[] digestBroker(String msg, byte[] nonce) throws NoSuchAlgorithmException{
+    // get a message digest object using the specified algorithm
+    MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
+    System.out.println(messageDigest.getProvider().getInfo());
+
+    System.out.println("Computing digest ...");
+    byte[] plainBytes = msg.getBytes();
+    byte[] plainPlusNonce = new byte[nonce.length + plainBytes.length];
+    
+    //concat: nonce + message
+    System.arraycopy(nonce, 0, plainPlusNonce, 0, nonce.length);
+    System.arraycopy(plainBytes, 0, plainPlusNonce, nonce.length, plainBytes.length);
+    messageDigest.update(plainPlusNonce);
+    return messageDigest.digest();
+  }
+
+  public static byte[] digestTransporter(String msg, byte[] nonce, String TransporterName) throws NoSuchAlgorithmException{
+    // get a message digest object using the specified algorithm
+    MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
+    System.out.println(messageDigest.getProvider().getInfo());
+
+    System.out.println("Computing digest ...");
+    byte[] plainBytes = msg.getBytes();
+    byte[] nameBytes = TransporterName.getBytes();
+    byte[] plainPlusNonceAndName = new byte[nonce.length + plainBytes.length + nameBytes.length];
+    
+    //concat: name + nonce + message
+    System.arraycopy(nameBytes, 0, plainPlusNonceAndName, 0, nameBytes.length);
+    System.arraycopy(nonce, 0, plainPlusNonceAndName, nameBytes.length, nonce.length);
+    System.arraycopy(plainBytes, 0, plainPlusNonceAndName, nonce.length, plainBytes.length);
+    messageDigest.update(plainPlusNonceAndName);
+    return messageDigest.digest();
+  }
+
+  private static byte[] readFile(String path) throws FileNotFoundException, IOException {
+    FileInputStream fis = new FileInputStream(path);
+    byte[] content = new byte[fis.available()];
+    fis.read(content);
+    fis.close();
+    return content;
+  }
+
+  public static byte[] decryptMessage(PublicKey publicKey, byte[] message) throws Exception{
+    // get an RSA cipher object
+    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+    // decrypt the ciphered digest using the public key
+    cipher.init(Cipher.DECRYPT_MODE, publicKey);
+    return cipher.doFinal(message);
+  }
+
+  public static Key getKey(String path) throws Exception{
+    byte[] privEncoded = readFile(path);
+    PKCS8EncodedKeySpec privSpec = new PKCS8EncodedKeySpec(privEncoded);
+    KeyFactory keyFacPriv = KeyFactory.getInstance("RSA");
+    return keyFacPriv.generatePrivate(privSpec);
+  }
+
+  /**
+   * auxiliary method to calculate new digest from text and compare it to the
+   * to deciphered digest
+   */
+  public static boolean verifyDigitalSignature(byte[] cipherDigest, byte[] bytes, PublicKey publicKey)
+      throws Exception {
+
+    // verify the signature with the public key
+    Signature sig = Signature.getInstance("SHA1WithRSA");
+    sig.initVerify(publicKey);
+    sig.update(bytes);
+    try {
+      return sig.verify(cipherDigest);
+    } catch (SignatureException se) {
+      System.err.println("Caught exception while verifying signature " + se);
+      return false;
+    }
+  }
 }
