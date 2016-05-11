d539bf96e767c749d72edbddd7f0b5d0468d8107
Lídia Freitas <lidiamcfreitas@gmail.com>
2016-05-11 18:29:00 +0100

    broker-cli working

--- broker-ws-cli/src/main/java/pt/upa/broker/BrokerClientApplication.java
+++ broker-ws-cli/src/main/java/pt/upa/broker/BrokerClientApplication.java
@@ -1,5 +1,7 @@
 package pt.upa.broker;
 
+import java.util.Scanner;
+
 import pt.upa.broker.ws.cli.BrokerClient;
 
 public class BrokerClientApplication {
@@ -17,8 +19,26 @@ public class BrokerClientApplication {
     BrokerClient client = new BrokerClient(args[0], args[1]);
     
     System.out.println("Remote call ...");
+    Scanner sc = new Scanner(System.in);
+    String input = "";
+    String result = "";
+    while(true){
+      input = sc.nextLine();
+      if(input.equals("quit")){
+        break;
+      }
+      if(input.equals("ping")){
+        result = client.ping("Client");
+        System.out.println(result);
+      } else if(input.equals("request")){
+        result = client.requestTransport("Coimbra","Lisboa", 49);
+        System.out.println(result);
+      }
+      // while(sc.hasNextLine()) System.out.println(sc.nextLine());
+    }
+    sc.close();
     //String result = client.ping("Client");
-    String result = client.requestTransport("Coimbra","Lisboa", 49);
-    System.out.println(result);
+    //String result = client.requestTransport("Coimbra","Lisboa", 49);
+    //System.out.println(result);
   }
 }
--- broker-ws-cli/src/main/java/pt/upa/broker/ws/cli/BrokerClient.java
+++ broker-ws-cli/src/main/java/pt/upa/broker/ws/cli/BrokerClient.java
@@ -2,11 +2,14 @@ package pt.upa.broker.ws.cli;
 
 import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
 
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import javax.xml.registry.JAXRException;
 import javax.xml.ws.BindingProvider;
+import javax.xml.ws.WebServiceException;
 
 import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
 import pt.upa.broker.ws.BrokerPortType;
@@ -25,10 +28,12 @@ public class BrokerClient {
   private String endpointAddress;
   private BrokerPortType port;
   private String uddiURL;
+  private String name;
   
   
   public BrokerClient(String uddiURL, String name) throws JAXRException, UnknownServiceException{
     this.uddiURL = uddiURL;
+    this.name = name;
     setUDDINaming(uddiURL);
     setEndpointAddresss(name);
     createPort();
@@ -67,28 +72,116 @@ public class BrokerClient {
     BindingProvider bindingProvider = (BindingProvider) port;
     Map<String, Object> requestContext = bindingProvider.getRequestContext();
     requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
+    
+    int connectionTimeout = 6000;
+    // The connection timeout property has different names in different versions of JAX-WS
+    // Set them all to avoid compatibility issues
+    final List<String> CONN_TIME_PROPS = new ArrayList<String>();
+    CONN_TIME_PROPS.add("com.sun.xml.ws.connect.timeout");
+    CONN_TIME_PROPS.add("com.sun.xml.internal.ws.connect.timeout");
+    CONN_TIME_PROPS.add("javax.xml.ws.client.connectionTimeout");
+    // Set timeout until a connection is established (unit is milliseconds; 0 means infinite)
+    for (String propName : CONN_TIME_PROPS)
+        requestContext.put(propName, connectionTimeout);
+    System.out.printf("Set connection timeout to %d milliseconds%n", connectionTimeout);
+
+    int receiveTimeout = 6000;
+    // The receive timeout property has alternative names
+    // Again, set them all to avoid compability issues
+    final List<String> RECV_TIME_PROPS = new ArrayList<String>();
+    RECV_TIME_PROPS.add("com.sun.xml.ws.request.timeout");
+    RECV_TIME_PROPS.add("com.sun.xml.internal.ws.request.timeout");
+    RECV_TIME_PROPS.add("javax.xml.ws.client.receiveTimeout");
+    // Set timeout until the response is received (unit is milliseconds; 0 means infinite)
+    for (String propName : RECV_TIME_PROPS)
+        requestContext.put(propName, 1000);
+    System.out.printf("Set receive timeout to %d milliseconds%n", receiveTimeout);
   }
   
-  public String ping(String string) {
-    return port.ping(string);
+  public String ping(String string) throws JAXRException, UnknownServiceException {
+    
+    String result = "";
+    try{
+      result = port.ping(string);
+    } catch(WebServiceException wse){
+      System.out.println("Caught: " + wse);
+      Throwable cause = wse.getCause();
+      if (cause != null && cause instanceof SocketTimeoutException) {
+          System.out.println("The cause was a timeout exception: " + cause);
+      }
+      this.setEndpointAddresss(this.name);
+      this.createPort();
+      return port.ping(string);
+    }
+    return result;
   }
   
   public String requestTransport(String origin, String destination, int price) throws 
   InvalidPriceFault_Exception, UnavailableTransportFault_Exception, 
-  UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception{
-    return port.requestTransport(origin, destination, price);
+  UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception, JAXRException, UnknownServiceException{
+    String result = "";
+    try{
+      result = port.requestTransport(origin, destination, price);
+    } catch(WebServiceException wse){
+      System.out.println("Caught: " + wse);
+      Throwable cause = wse.getCause();
+      if (cause != null && cause instanceof SocketTimeoutException) {
+          System.out.println("The cause was a timeout exception: " + cause);
+      }
+      this.setEndpointAddresss(this.name);
+      this.createPort();
+      return port.requestTransport(origin, destination, price);
+    }
+    return result;
   }
   
-  public  TransportView viewTransport(String id) throws UnknownTransportFault_Exception{
-    return port.viewTransport(id);
+  public TransportView viewTransport(String id) throws UnknownTransportFault_Exception, JAXRException, UnknownServiceException{
+    TransportView result =  null;
+    try{
+      result =  port.viewTransport(id);
+    } catch(WebServiceException wse){
+      System.out.println("Caught: " + wse);
+      Throwable cause = wse.getCause();
+      if (cause != null && cause instanceof SocketTimeoutException) {
+          System.out.println("The cause was a timeout exception: " + cause);
+      }
+      this.setEndpointAddresss(this.name);
+      this.createPort();
+      return port.viewTransport(id);
+    }
+    return result;
   }
   
-  public  List<TransportView> listTransports(){
-    return port.listTransports();
+  public  List<TransportView> listTransports() throws JAXRException, UnknownServiceException{
+    List<TransportView> result = null;
+    try{
+      result =  port.listTransports();
+    } catch(WebServiceException wse){
+      System.out.println("Caught: " + wse);
+      Throwable cause = wse.getCause();
+      if (cause != null && cause instanceof SocketTimeoutException) {
+          System.out.println("The cause was a timeout exception: " + cause);
+      }
+      this.setEndpointAddresss(this.name);
+      this.createPort();
+      return port.listTransports();
+    }
+    return result;
   }
   
-  public void clearTransports(){
-    port.clearTransports();
+  public void clearTransports() throws JAXRException, UnknownServiceException{
+    try{
+      port.clearTransports();
+    } catch(WebServiceException wse){
+      System.out.println("Caught: " + wse);
+      Throwable cause = wse.getCause();
+      if (cause != null && cause instanceof SocketTimeoutException) {
+          System.out.println("The cause was a timeout exception: " + cause);
+      }
+      this.setEndpointAddresss(this.name);
+      this.createPort();
+      port.clearTransports();
+    }
   }
   
 }
